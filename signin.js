5dc227fc040da976207a052ce82c437dabc1fc38
pdt-timchinchen <35146601+pdt-timchinchen@users.noreply.github.com>
2020-03-19 15:36:03 +0000

    Update signin.js

--- signin.js
+++ signin.js
@@ -50,7 +50,7 @@ $('#inputPassword').keypress(function(e) {
 });
 
 //Exercise Pt2 - FIX ME!
-$('signin-button').on('click', function() {
+$('#signin-button').on('click', function() {
 	
    alert('here');
 	
