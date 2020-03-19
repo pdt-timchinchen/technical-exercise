79beb651112332c37fe8a4f029e47b90a7763e44
pdt-timchinchen <35146601+pdt-timchinchen@users.noreply.github.com>
2020-03-19 15:33:30 +0000

    Update signin.js

--- signin.js
+++ signin.js
@@ -51,6 +51,9 @@ $('#inputPassword').keypress(function(e) {
 
 //Exercise Pt2 - FIX ME!
 $('signin-button').on('click', function() {
+	
+   alert('here');
+	
     if (email != $('#inputEmail').val()) count = 0;
     count++;
     email = $('#inputEmail').val();
